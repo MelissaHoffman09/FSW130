83514046eefdfc05a271542868377704057fb2df
Melissa Hoffman <73321224+MelissaHoffman09@users.noreply.github.com>
2021-05-02 23:09:22 -0700

    week3

---
+++ week3/timer/timer/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./styles.css"
+import Timer from "./components/Timer"
+
+function App() {
+    return (
+        <div>
+          <h1 className="title">Timer</h1>
+          <Timer />
+        </div>
+    )
+}
+
+export default App
