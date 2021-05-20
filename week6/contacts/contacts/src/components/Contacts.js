1ccae48e430b059800c4bda1a55a4c6f9a00e450
Melissa Hoffman <73321224+MelissaHoffman09@users.noreply.github.com>
2021-05-20 00:44:24 -0700

    week6-2

--- week6/contacts/contacts/src/components/Contacts.js
+++ week6/contacts/contacts/src/components/Contacts.js
@@ -6,7 +6,7 @@ function Contact (props) {
     <div id={props.id}>
       <h1>Name: {props.name}</h1>
       <h2>Phone: {props.phone}</h2>
-      <h2>Email:{props.email}</h2>
+      <h2>Email: {props.email}</h2>
       <button onClick={props.onClick} className="button">Remove</button>
     </div>
   )
